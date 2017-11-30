45f4f7e72fc1c382293e7b47bad14c886d470ca6
TS Chia <tiongsun@gmail.com>
2017-11-30 17:10:58 +0000

    Update call.py

--- call.py
+++ call.py
@@ -12,13 +12,14 @@ input2 = sys.argv[2]# ref fasta
 
 input_csv = input1 # columns = position, A, C, G, T
 refseq = input2
+
 output_filename = input2 + '_call.csv'
+depth_checker_filename = input2 + '_depth_checker.csv'
 
 #==============================================================================
 # Calculation
 #==============================================================================
-base = [list(i.seq) for i in SeqIO.parse(refseq,'fasta')]
-refpos = base[0]
+refpos = [list(i.seq) for i in SeqIO.parse(refseq,'fasta')][0]
 
 output, poslist = [],[]
 min_read_depth  = 1
@@ -30,18 +31,17 @@ with open(input_csv) as f:
             count += 1
             readrow = [int(float(x)) for x in row.split()[:5]]
             position = readrow[0]
-            ref = refpos[position-1].upper() # -1 for python 0-index
+            ref = refpos[position-1].upper() 
             
             rowdic = {'A': readrow[1], 'C':readrow[2],'G':readrow[3],'T':readrow[4]}
             sumall = sum((rowdic.values()))
-            
-            poslist.append(position)
+            poslist.append([position, sumall])
 
 # no read depth
 
             if position != count:
                 try:
-                    out = {'Position': str(poslist[-2]) + ' to ' +str(position),
+                    out = {'Position': str(poslist[0][-2]) + ' to ' +str(position),
                            'Mutation': 'Read Depth = 0',
                            'Ref Base Rate':  '0',
                            'Read Base Rate': '0',
@@ -104,9 +104,25 @@ with open(input_csv) as f:
 
 df = pd.DataFrame.from_dict(output)
 try: 
-    df = df[['Position','Mutation','Ref Base Rate', 'Read Base Rate','Read Depth']]
+    df = df[['Position','Mutation','Ref Base Rate', 'Read Base Rate','Read Depth']].set_index('Position')
 except KeyError:
     pass
 df.to_csv(output_filename)
 
-            
+# Depth checker
+depth_checker = []
+for i in range(1,len(poslist)):
+    if poslist[i-1][1] > (poslist[i][1]* 5):
+        percent = (1 - (poslist[i][1]/poslist[i-1][1]))*100
+        depth_checker.append({'Position': poslist[i][0],
+                              'Note': 'Read depth drops by {} percent'.format(round(percent,0))})
+    elif (poslist[i-1][1])*2 < poslist[i][1]:
+        percent = ((poslist[i][1]/poslist[i-1][1])-1)*100
+        depth_checker.append({'Position': poslist[i][0],
+                              'Note': 'Read depth increases by {} percent'.format(round(percent,0))})
+
+df2 = pd.DataFrame.from_dict(depth_checker)
+df2 = df2[['Position', 'Note']].set_index('Position')
+df2.to_csv(depth_checker_filename)        
+        
+    
